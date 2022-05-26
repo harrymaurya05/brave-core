0185fefb0a322d790aaf7e782255f221614071fe
Josh Leonard <jleonard@brave.com>
2022-05-26 10:23:45 -0600

    feat: add restore from recovery phrase screen

--- components/brave_wallet_ui/stories/locale.ts
+++ components/brave_wallet_ui/stories/locale.ts
@@ -210,9 +210,10 @@ provideStrings({
   braveWalletRestoreLegacyCheckBox: 'Import from legacy Brave crypto wallets?',
   braveWalletRestoreFormText: 'New Password',
 
-  // Tool Tips
+  // Clipboard
   braveWalletToolTipCopyToClipboard: 'Copy to Clipboard',
   braveWalletToolTipCopiedToClipboard: 'Copied!',
+  braveWalletPasteFromClipboard: 'Paste from clipboard',
 
   // Accounts Tab
   braveWalletAccountsPrimary: 'Primary crypto accounts',

df562949620b5fd61b766a1f449b0fb3a8b4d832
Anirudha Bose <anirudha.bose@alumni.cern>
2022-06-09 08:53:54 +0530

    feat(wallet): integrate Jupiter API for Solana swaps

--- components/brave_wallet/browser/brave_wallet_constants.h
+++ components/brave_wallet/browser/brave_wallet_constants.h
@@ -656,7 +656,7 @@ constexpr webui::LocalizedString kLocalizedStrings[] = {
     {"braveWalletHelpCenter", IDS_BRAVE_WALLET_HELP_CENTER},
     {"braveWalletHelpCenterText", IDS_BRAVE_WALLET_HELP_CENTER_TEXT}};
 
-// Swap constants
+// 0x swap constants
 constexpr char kRopstenSwapBaseAPIURL[] = "https://ropsten.api.0x.org/";
 // As of 22-02-2022, this address is controlled by @onyb.
 constexpr char kRopstenFeeRecipient[] =
@@ -672,6 +672,12 @@ constexpr char kBuyTokenPercentageFee[] = "0.00875";
 constexpr char kFeeRecipient[] = "0xbd9420A98a7Bd6B89765e5715e169481602D9c3d";
 constexpr char kAffiliateAddress[] =
     "0xbd9420A98a7Bd6B89765e5715e169481602D9c3d";
+
+// Jupiter swap constants
+constexpr char kSolanaSwapBaseAPIURL[] = "https://quote-api.jup.ag/";
+constexpr char kSolanaBuyTokenFeeBps[] = "85";
+constexpr char kSolanaFeeRecipient[] =
+    "8eekKfUAGSJbq3CdA2TmHb8tKuyzd5gtEas3MYAtXzrT";
 
 constexpr int64_t kBlockTrackerDefaultTimeInSeconds = 20;
 

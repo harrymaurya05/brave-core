f5cc5bb254b901be5dd229f9a84ee7d69abbda71
Jay Harris <jay.harris@outlook.co.nz>
2022-06-21 10:48:02 +1200

    Spacing

--- components/brave_new_tab_ui/components/braveNews/FeedCard.tsx
+++ components/brave_new_tab_ui/components/braveNews/FeedCard.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
-import styled from "styled-components";
-import { Publisher, togglePublisher } from "../../api/brave_news";
+import styled, { keyframes } from "styled-components";
+import { isPublisherEnabled, Publisher, togglePublisher } from "../../api/brave_news";
 import Flex from "../Flex";
 import FollowButton from "./FollowButton";
+import { Heart, HeartOutline } from "./Icons";
 
 export const Container = styled(Flex)`
+    position: relative;
 `;
 
 export const Card = styled('div') <{ backgroundColor?: string, backgroundImage?: string }>`
@@ -26,6 +28,25 @@ export const Name = styled.span`
     font-weight: 600;
 `
 
+const Pulse = keyframes`
+    0% { opacity: 0; }
+    50% { opacity: 1; }
+    100% { opacity: 0; }
+`
+
+export const HeartOverlay = styled('div')<{ following: boolean }>`
+    pointer-events: none;
+    background: white;
+    color: #aeb1c2;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+
+    animation: ${p => p.following ? Pulse : Pulse} 2s ease-in-out;
+`
+
 export default function FeedCard(props: {
     publisher: Publisher;
     backgroundColor?: string;
@@ -33,12 +54,16 @@ export default function FeedCard(props: {
     following: boolean;
     name: string;
 }) {
+    const following = isPublisherEnabled(props.publisher);
     return <Container direction="column" gap={8}>
         <Card backgroundColor={props.backgroundColor} backgroundImage={props.backgroundImage}>
-            <StyledFollowButton following={props.following} onClick={() => togglePublisher(props.publisher)}/>
+            <StyledFollowButton following={props.following} onClick={() => togglePublisher(props.publisher)} />
         </Card>
         <Name>
             {props.name}
         </Name>
+        <HeartOverlay following={following}>
+            {following ? Heart : HeartOutline}
+        </HeartOverlay>
     </Container>
 }

aae3de81d75842c1d382ddc0da0222cfbd16a4f0
Jay Harris <jay.harris@outlook.co.nz>
2022-06-20 16:17:36 +1200

    Make source cards work for BraveNews

--- components/brave_new_tab_ui/components/braveNews/SourceCard.tsx
+++ components/brave_new_tab_ui/components/braveNews/SourceCard.tsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 import * as React from 'react';
 import Flex from "../Flex";
+import { useBraveNews } from './Context';
 
-const Container = styled(Flex)<{ background?: string }>`
+const Container = styled(Flex) <{ background?: string }>`
     font-weight: 600;
     font-size: 14px;
     border-radius: 8px;
@@ -16,7 +17,15 @@ const Container = styled(Flex)<{ background?: string }>`
 `
 
 export default function SourceCard(props: { icon: React.ReactNode, text: string, id: string, background?: string }) {
-    return <Container direction='column' justify='end' align='start' background={props.background}>
+    const { setPage } = useBraveNews();
+
+    return <Container
+        onClick={() => setPage(`category/${props.id}`)}
+        direction='column'
+        justify='end'
+        align='start'
+        background={props.background}
+    >
         {props.icon}
         {props.text}
     </Container>

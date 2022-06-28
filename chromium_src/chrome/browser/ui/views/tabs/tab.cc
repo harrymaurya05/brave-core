a3cb3c1af3411fde2c0005d7079cac1ad97f0326
Jay Harris <jay.harris@outlook.co.nz>
2022-06-29 11:29:03 +1200

    Fix build failures

--- chromium_src/chrome/browser/ui/views/tabs/tab.cc
+++ chromium_src/chrome/browser/ui/views/tabs/tab.cc
@@ -3,6 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "brave/components/brave_tabs/brave_tab_prefs.h"
 #include "chrome/browser/ui/views/tabs/tab.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/prefs/pref_service.h"

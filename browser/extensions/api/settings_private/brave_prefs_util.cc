f8ac57c0ce7d54b1e98aa0790dd85a25e890456b
Jay Harris <jay.harris@outlook.co.nz>
2022-06-29 10:18:38 +1200

    Work on preview cards

--- browser/extensions/api/settings_private/brave_prefs_util.cc
+++ browser/extensions/api/settings_private/brave_prefs_util.cc
@@ -176,6 +176,8 @@ const PrefsUtil::TypedPrefMap& BravePrefsUtil::GetAllowlistedKeys() {
       settings_api::PrefType::PREF_TYPE_BOOLEAN;
   (*s_brave_allowlist)[brave_tabs::kTabHoverCardTooltipsEnabled] =
       settings_api::PREF_TYPE_BOOLEAN;
+  (*s_brave_allowlist)[brave_tabs::kTabHoverCardPreviewEnabled] =
+      settings_api::PREF_TYPE_BOOLEAN;
 #if BUILDFLAG(ENABLE_BRAVE_VPN)
   (*s_brave_allowlist)[brave_vpn::prefs::kBraveVPNShowButton] =
       settings_api::PrefType::PREF_TYPE_BOOLEAN;

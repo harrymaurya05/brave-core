20843463a5f654f2d2d0d58f8a572d1a1a15f80a
boocmp <boocmp@yandex.ru>
2022-07-01 21:08:07 +0700

    Moved Tor settings in the separate section.

--- browser/resources/settings/brave_routes.js
+++ browser/resources/settings/brave_routes.js
@@ -71,5 +71,5 @@ export default function addBraveRoutes(r) {
   } else if (!isGuest) {
     console.error('[Brave Settings Overrides] Could not move safety check route to advanced route', r)
   }
-  r.TOR = r.PRIVACY.createChild('tor')
+  r.TOR = r.BASIC.createSection('/tor', 'tor')
 }

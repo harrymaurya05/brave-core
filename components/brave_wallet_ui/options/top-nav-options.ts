7ab42ecf1171066296abb7189687d2bc27eecce9
William Muli <willi.wambu@gmail.com>
2022-07-06 09:53:30 +0300

    chore: fix storybook views

--- components/brave_wallet_ui/options/top-nav-options.ts
+++ components/brave_wallet_ui/options/top-nav-options.ts
@@ -4,7 +4,7 @@
 // you can obtain one at http://mozilla.org/MPL/2.0/.
 
 import { TopTabNavObjectType } from '../constants/types'
-import { getLocale } from '../../common/locale'
+import { getLocale } from '$web-common/locale'
 
 export const TopNavOptions = (): TopTabNavObjectType[] => [
   {

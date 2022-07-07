b3adb5b967c48a5b06a31323e5597b314bbd2690
Brandon T <JustBrandonT@gmail.com>
2022-07-06 23:16:53 -0400

    Syncing To Desktop Make Tab Syncing from iOS work and add API for syncing tabs. Refactor Tab into its own class to handle titles and urls. Proper handling of Navigation and WebState Observers.

--- ios/browser/api/opentabs/brave_opentabs_api.mm
+++ ios/browser/api/opentabs/brave_opentabs_api.mm
@@ -4,9 +4,13 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/ios/browser/api/opentabs/brave_opentabs_api.h"
+#import <WebKit/WebKit.h>
 
 #include "base/bind.h"
 #include "base/strings/sys_string_conversions.h"
+
+#include "components/sync_sessions/open_tabs_ui_delegate.h"
+#include "components/sync_sessions/session_sync_service.h"
 
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/ui/recent_tabs/synced_sessions.h"
@@ -17,6 +21,48 @@
 
 #include "brave/ios/browser/api/opentabs/brave_opentabs_observer.h"
 #include "brave/ios/browser/api/opentabs/opentabs_session_listener_ios.h"
+
+#include "ios/chrome/browser/application_context.h"
+#include "ios/chrome/browser/browser_state/chrome_browser_state.h"
+#include "ios/chrome/browser/browser_state/chrome_browser_state_manager.h"
+#include "ios/chrome/browser/main/browser_list.h"
+#include "ios/chrome/browser/main/browser_list_factory.h"
+#import "ios/chrome/browser/tabs/synced_window_delegate_browser_agent.h"
+#import "ios/chrome/browser/web_state_list/web_state_list.h"
+#include "ios/chrome/browser/tabs/tab_helper_util.h"
+#include "ios/chrome/browser/sync/ios_chrome_synced_tab_delegate.h"
+#include "ios/chrome/browser/web_state_list/web_state_opener.h"
+#import "ios/chrome/browser/web/sad_tab_tab_helper.h"
+#import "ios/chrome/browser/sessions/ios_chrome_session_tab_helper.h"
+#include "ios/chrome/browser/complex_tasks/ios_task_tab_helper.h"
+#include "ios/chrome/browser/ntp/new_tab_page_tab_helper.h"
+#include "ios/chrome/browser/open_in/open_in_tab_helper.h"
+#include "ios/chrome/browser/web/session_state/web_session_state_tab_helper.h"
+#import "ios/web/navigation/navigation_manager_impl.h"
+#include "ios/web/web_state/web_state_impl.h"
+
+#import "ios/chrome/browser/main/browser.h"
+#import "ios/chrome/browser/main/browser_list.h"
+#import "ios/chrome/browser/main/browser_list_factory.h"
+#import "ios/chrome/browser/app_launcher/app_launcher_tab_helper.h"
+#import "ios/chrome/browser/snapshots/snapshot_tab_helper.h"
+#import "ios/web/web_state/ui/crw_web_view_navigation_proxy.h"
+
+#import "ios/chrome/browser/web_state_list/tab_insertion_browser_agent.h"
+#import "ios/chrome/browser/snapshots/snapshot_browser_agent.h"
+#import "ios/chrome/browser/tabs/closing_web_state_observer_browser_agent.h"
+#import "ios/chrome/browser/tabs/synced_window_delegate_browser_agent.h"
+#import "ios/chrome/browser/device_sharing/device_sharing_browser_agent.h"
+#import "ios/chrome/browser/app_launcher/app_launcher_browser_agent.h"
+#import "ios/chrome/browser/tabs/tab_parenting_browser_agent.h"
+#import "ios/chrome/browser/web/web_navigation_browser_agent.h"
+#import "ios/chrome/browser/web/web_state_delegate_browser_agent.h"
+#import "ios/chrome/browser/policy/policy_watcher_browser_agent.h"
+#import "ios/chrome/browser/send_tab_to_self/send_tab_to_self_browser_agent.h"
+#import "ios/chrome/browser/main/browser_impl.h"
+
+#include "brave/ios/browser/api/opentabs/brave_tab.h"
+
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -153,6 +199,12 @@ SyncDeviceType SyncEnumsDeviceTypeFromSyncDeviceType(
 @end
 
 #pragma mark - BraveOpenTabsAPI
+
+namespace web {
+
+
+}  // namespace web
+
 
 @interface BraveOpenTabsAPI () {
   // Session Sync Service is needed in order to receive session details from different instances

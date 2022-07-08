52e18c1b37122ff250f84a68e47c893cdcc14e0d
Jay Harris <jay.harris@outlook.co.nz>
2022-07-08 12:17:14 +1200

    Make pref editable

--- components/brave_new_tab_ui/storage/new_tab_storage.ts
+++ components/brave_new_tab_ui/storage/new_tab_storage.ts
@@ -19,6 +19,7 @@ export const defaultState: NewTab.State = {
   showBackgroundImage: false,
   showStats: false,
   showToday: false,
+  showBraveNewsButton: false,
   showClock: false,
   clockFormat: '',
   showTopSites: false,
